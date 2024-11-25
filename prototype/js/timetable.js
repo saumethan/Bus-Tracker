ccfb4b302c1413b5f9a60aa6e4785d3997ba7d70
XavierFlockton <x.flockton@rgu.ac.uk>
2024-11-25 11:59:14 +0000

    added timetable functionality

---
+++ prototype/js/timetable.js
@@ -0,0 +1,15 @@
+function generateLink() {
+
+    var service = document.getElementById("service").value;
+    var area = document.getElementById("area").value;
+    var route = document.getElementById("route").value;
+
+
+
+    var finalURL = `${service}?opco=${area}&service=${route}&day=mf&print=pdf`;
+
+    var pdfContainer = document.getElementById("pdfContainer");
+    pdfContainer.innerHTML = `
+        <iframe src="${finalURL}" title="Bus Timetable PDF"></iframe>
+    `;
+}
