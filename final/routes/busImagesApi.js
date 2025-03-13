8546acd70892ba5fce023d907a575707eb328ab4
owenrgu <o.meade@rgu.ac.uk>
2025-03-13 14:15:50 +0000

    Fix package errors

--- final/routes/busImagesApi.js
+++ final/routes/busImagesApi.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const router = express.Router();
 const { createCanvas, loadImage } = require("canvas");
-const path = require("path");
 
 // Functions
 // Draw direction triangle based on bearing
