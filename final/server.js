bb0a579defe7013edb750085ebfc79eddbb39127
owenrgu <o.meade@rgu.ac.uk>
2025-03-13 11:05:56 +0000

    Add API endpoint to fetch a bus image

--- final/server.js
+++ final/server.js
@@ -9,6 +9,7 @@ app.use(express.static("public"));
 // Import API routes 
 const busRoutes = require("./routes/busRoutesApi");
 const stopRoutes = require("./routes/stopRoutesApi");
+const busImageRoutes = require("./routes/busImagesApi");
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
@@ -42,6 +43,7 @@ app.get("/timetable", function(req, res) {
 // Use the API routes (from apiRoutes.js)
 app.use("/api/buses", busRoutes);
 app.use("/api/stops", stopRoutes);
+app.use("/api/busimages", busImageRoutes);
 
 // 404 page
 app.use(function(req, res, next) {
