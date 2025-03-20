83419d8c8296bea9c1464a9484abab3b3c412fd1
JoshuaNewton-rgu <j.newton@rgu.ac.uk>
2025-03-20 11:47:49 +0000

    Added create page and made changes to settings and login page

---
+++ final/routes/create.js
@@ -0,0 +1,51 @@
+// create.js
+
+const express = require("express");
+const router = express.Router();
+
+// MongoDB connection setup
+const MongoClient = require("mongodb-legacy").MongoClient;
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
+const dbName = "User_Profiles";
+let db;
+
+// Connect to database
+async function connectDB() {
+    try {
+        await client.connect();
+        db = client.db(dbName);
+        console.log("Connected to the database");
+    } catch (error) {
+        console.error("Database connection failed", error);
+    }
+}
+connectDB();
+
+// SERVER ENDPOINT: create page
+router.get("/", function(req, res) {
+    res.render("pages/create");
+});
+
+
+//Create User Account
+    app.post('/createUser', function(req,res){
+        //Checks that a user isnt already logged in instead of creating a new account
+        if(!req.session.loggedin){res.redirect('/login');return}
+
+    })
+
+var datatostore={
+    "name":{"title":req.body.title, "first":req.body.first},
+    "login":{"username":req.body.email, "password": req.body.password},
+    "registered": Date()
+}
+
+db.collection('users').insertOne(datatostore, function(err, result){
+    if(err) throw err
+    console.log("Saved to database")
+    res.redirect('/')
+})
+
+
+module.exports = router;
