f66c01c9901a30f2de86d66abbc6ee3fd0b09bd7
JoshuaNewton-rgu <j.newton@rgu.ac.uk>
2025-03-20 12:01:31 +0000

    error fixes

--- final/routes/create.js
+++ final/routes/create.js
@@ -1,3 +1,33 @@
+// create.js
+
+const express = require("express");
+const router = express.Router();
+
+// MongoDB connection setup
+const MongoClient = require("mongodb-legacy").MongoClient;
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
+const dbName = "User_Profiles";
+let db;
+
+// Connect to database
+async function connectDB() {
+    try {
+        await client.connect();
+        db = client.db(dbName);
+        console.log("Connected to the database");
+    } catch (error) {
+        console.error("Database connection failed", error);
+    }
+}
+connectDB();
+
+// SERVER ENDPOINT: create page
+router.get("/", function(req, res) {
+    res.render("pages/create");
+});
+
+
 // Create User Account
 router.post('/createUser', async function(req, res) {
     try {
@@ -25,3 +55,6 @@ router.post('/createUser', async function(req, res) {
         res.status(500).send("Failed to create account");
     }
 });
+
+
+module.exports = router;
