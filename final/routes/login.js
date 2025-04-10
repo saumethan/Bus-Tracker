c03fb87f2eba1d96c38f899a190bf7eede0bbf59
JoshuaNewton-rgu <j.newton@rgu.ac.uk>
2025-04-10 18:01:37 +0100

    testing more thuroughly

--- final/routes/login.js
+++ final/routes/login.js
@@ -38,8 +38,13 @@ router.post('/userlogin', async function(req, res) {
         });
 
         if (!result) {
-            console.log("No User Found");
-            return res.status(401).send("No User with that name found");
+            //console.log("No User Found");
+            //return res.status(401).send("No User with that name found");
+            res.render("pages/login", {
+                page: "login",
+                loggedIn: false,
+                error: "No user found with that name"
+            });
         }
 
         if (result.login.password === userPass) {
@@ -48,6 +53,8 @@ router.post('/userlogin', async function(req, res) {
             console.log("Logged in:", req.session.loggedin);
             res.redirect('/');
         } else {
+            //console.log("Incorrect Password");
+            //res.status(401).send("Incorrect Password, please try again");
             console.error("Password Error", error);
             res.render("pages/login", {
                 page: "login",
@@ -56,11 +63,16 @@ router.post('/userlogin', async function(req, res) {
             });
         }
     } catch (error) {
-        console.error("Login Error", error);
-        res.status(500).send("Failed to log in at this time");
+        //console.error("Login Error", error);
+        //res.status(500).send("Failed to log in at this time");
+        res.render("pages/login", {
+            page: "login",
+            loggedIn: false,
+            error: "Failed to log in at this time"
+        });
     }
 });
-
+//Ethans settings for zoom level
 router.post('/userSettings', async function(req, res) {
     if (!req.session || !req.session.loggedin) {
         return res.status(401).send("Unauthorized: Please log in first.");
