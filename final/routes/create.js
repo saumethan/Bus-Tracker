8b02dd848b04d8b72010e811bbe12abc8c1df93b
saumethan272 <e.saum@rgu.ac.uk>
2025-04-15 13:54:59 +0100

    removed unused code

--- final/routes/create.js
+++ final/routes/create.js
@@ -49,15 +49,15 @@ router.post("/createUser", async function(req, res) {
         const email = req.body.email.trim();
 
         // email rule - letters, numbers, hyphens, apostrophes, must include at least one dot
-        const validEmail = /^[a-zA-Z0-9\-'.]*\.[a-zA-Z0-9\-'.]+$/;
-        if (!validEmail.test(email)) {
-            console.log("Invalid email format attempt:", email);
-            return res.render("pages/create", {
-                page: "create",
-                loggedIn: false,
-                error: "Email can only use letters, numbers, - and '"
-            });
-        }
+        // const validEmail = /^[a-zA-Z0-9\-'.]*\.[a-zA-Z0-9\-'.]+$/;
+        // if (!validEmail.test(email)) {
+        //     console.log("Invalid email format attempt:", email);
+        //     return res.render("pages/create", {
+        //         page: "create",
+        //         loggedIn: false,
+        //         error: "Email can only use letters, numbers, - and '"
+        //     });
+        // }
 
         // Check if the user already exists
         const existingUser = await db.collection("users").findOne({ "login.username": email });
